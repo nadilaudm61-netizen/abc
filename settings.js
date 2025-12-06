2d99dae025a588a1ad9e9df88c037cdca94522ce
nadilaudm61-netizen <nadilaudm61@gmail.com>
2025-12-06 19:17:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=0edb327a03a8b2d0 === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
